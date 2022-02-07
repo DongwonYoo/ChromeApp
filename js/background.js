a8dafb028c5557a141b41facc7cfb12cc511e7bb
Dongwon Yoo <thd301302@gmail.com>
2022-02-07 20:23:54 +0900

    toDo

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const backImage = images[Math.floor(Math.random() * images.length)]
+
+const bgImage =  document.createElement("img");
+
+bgImage.src = `img/${backImage}`;
+
+document.body.prepend(bgImage);
---
+++ js/todo.js
@@ -0,0 +1,59 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY ="todos"
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    
+    li.remove();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    li.id = newTodo.id;
+    const span = document.createElement("span");
+    span.innerText = newTodo.text; // sapn のテキストはhandleToDosubmitのnewTodo
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo)
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj =  {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj); 
+    paintToDo(newTodoObj);
+    saveToDos(); 
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;//Localstroage 새로고침하고 새로운값 입력시 유
+    parsedToDos.forEach(paintToDo);
+}
+
+function Filter(){
+    //지우고싶은것은 제외한 배열을 만든다.
+    
+}
